2c6cff196a0dfaaab20e3b85118c7998eb3a64bf
soufianemj1 <elmajati.s@gmail.com>
2022-05-19 09:54:11 +0100

    '.'

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import express, { Response, Request } from 'express';
+import compression from 'compression';
+import { limiter } from '@middlewares/limiter';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet(), compression(), limiter, express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hi Mom 👋',
+  });
+});
+
+const port = process.env.PORT || 5000;
+
+// listen to port
+app.listen(port, async () => {
+  console.log(`🚀 Server ready at: http://localhost:${port}`);
+});
